d0e8dc46e224ce8f56d8615e44e29e96ca316621
Rasmus Eneman <rasmus@eneman.eu>
2019-09-14 16:51:39 +0200

    Use rust style return type specification off ffi helpers

--- ffi_helpers.cpp
+++ ffi_helpers.cpp
@@ -4,7 +4,7 @@
 #include <miral/window_manager_tools.h>
 #include <wayland-server-core.h>
 
-extern "C" const char* window_specification_name(miral::WindowSpecification& specification)
+extern "C" auto window_specification_name(miral::WindowSpecification& specification) -> const char*
 {
     if (specification.name().is_set()) {
         return specification.name().value().c_str();
@@ -13,25 +13,25 @@ extern "C" const char* window_specification_name(miral::WindowSpecification& spe
     }
 }
 
-extern "C" int configure_window(miral::WindowInfo& window_info, MirWindowAttrib attrib, int value)
+extern "C" auto configure_window(miral::WindowInfo& window_info, MirWindowAttrib attrib, int value) -> int
 {
     std::shared_ptr<mir::scene::Surface> surface = window_info.window();
     return surface.get()->configure(attrib, value);
 }
 
-extern "C" void hide_window(miral::WindowInfo& window_info)
+extern "C" auto hide_window(miral::WindowInfo& window_info) -> void
 {
     std::shared_ptr<mir::scene::Surface> surface = window_info.window();
     surface.get()->hide();
 }
 
-extern "C" void show_window(miral::WindowInfo& window_info)
+extern "C" auto show_window(miral::WindowInfo& window_info) -> void
 {
     std::shared_ptr<mir::scene::Surface> surface = window_info.window();
     surface.get()->show();
 }
 
-extern "C" void* window_name(miral::WindowInfo& window_info)
+extern "C" auto window_name(miral::WindowInfo& window_info) -> void*
 {
     auto name = window_info.name();
     auto name_ptr = std::make_shared<std::string>();
@@ -39,27 +39,27 @@ extern "C" void* window_name(miral::WindowInfo& window_info)
     return static_cast<void*>(new std::shared_ptr<std::string>(name_ptr));
 }
 
-extern "C" const char* rust_get_string(std::shared_ptr<std::string> string)
+extern "C" auto rust_get_string(std::shared_ptr<std::string> string) -> const char*
 {
     return string.get()->c_str();
 }
 
-extern "C" void rust_drop_string(void* ptr)
+extern "C" auto rust_drop_string(void* ptr) -> void
 {
     std::shared_ptr<std::string> *value = static_cast<std::shared_ptr<std::string>*>(ptr);
     delete value;
 }
 
-extern "C" bool window_specification_has_parent(miral::WindowSpecification& specification)
+extern "C" auto window_specification_has_parent(miral::WindowSpecification& specification) -> bool
 {
     return specification.parent().is_set() && specification.parent().value().lock();
 }
-extern "C" bool window_info_has_parent(miral::WindowInfo& window_info)
+extern "C" auto window_info_has_parent(miral::WindowInfo& window_info) -> bool
 {
     return !!window_info.parent();
 }
 
-extern "C" void* get_active_window(miral::WindowManagerTools* tools)
+extern "C" auto get_active_window(miral::WindowManagerTools* tools) -> void*
 {
     auto window = tools->active_window();
     auto window_ptr = std::make_shared<miral::Window>();
@@ -67,7 +67,7 @@ extern "C" void* get_active_window(miral::WindowManagerTools* tools)
     return static_cast<void*>(new std::shared_ptr<miral::Window>(window_ptr));
 }
 
-extern "C" void* get_window_at(miral::WindowManagerTools* tools, mir::geometry::Point cursor)
+extern "C" auto get_window_at(miral::WindowManagerTools* tools, mir::geometry::Point cursor) -> void*
 {
     auto window = tools->window_at(cursor);
     auto window_ptr = std::make_shared<miral::Window>();
@@ -75,7 +75,7 @@ extern "C" void* get_window_at(miral::WindowManagerTools* tools, mir::geometry::
     return static_cast<void*>(new std::shared_ptr<miral::Window>(window_ptr));
 }
 
-extern "C" void select_active_window(miral::WindowManagerTools* tools, miral::Window const* hint)
+extern "C" auto select_active_window(miral::WindowManagerTools* tools, miral::Window const* hint) -> void
 {
     if (hint == NULL) {
         tools->select_active_window(miral::Window {});
@@ -84,23 +84,23 @@ extern "C" void select_active_window(miral::WindowManagerTools* tools, miral::Wi
     }
 }
 
-extern "C" miral::Window* rust_get_window(std::shared_ptr<miral::Window> window)
+extern "C" auto rust_get_window(std::shared_ptr<miral::Window> window) -> miral::Window*
 {
     return window.get();
 }
 
-extern "C" void rust_drop_window(void* ptr)
+extern "C" auto rust_drop_window(void* ptr) -> void
 {
     std::shared_ptr<miral::Window> *value = static_cast<std::shared_ptr<miral::Window>*>(ptr);
     delete value;
 }
 
-extern "C" bool client_is_alive(wl_client* client)
+extern "C" auto client_is_alive(wl_client* client) -> bool
 {
     return miral::application_for(client) != NULL;
 }
 
-extern "C" bool client_owns_window(wl_client* client, miral::WindowInfo const* window)
+extern "C" auto client_owns_window(wl_client* client, miral::WindowInfo const* window) -> bool
 {
     return miral::pid_of(miral::application_for(client)) == miral::pid_of(window->window().application());
 }
